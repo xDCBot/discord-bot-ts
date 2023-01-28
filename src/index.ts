78c3d814d937138de56aa4308f7c671b0c514055
Daniel Roberto <sdanirobert@outlook.com>
2023-01-28 18:06:50 -0300

    Code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import * as dotenv from 'dotenv';
-import * as Discord from 'discord.js';
-import { GatewayIntentBits } from 'discord.js';
+import Discord, { GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const client = new Discord.Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
-client.on('ready', () => {
-    console.log(`Connected as ${client.user!.tag}!`);
+client.on("ready", () => {
+  console.log(`Connected as ${client.user!.tag}!`);
 });
 
 client
-    .login(process.env.TOKEN)
-    .then(() => {
-        console.log('Successfully Connected!');
-    })
-    .catch((err) => {
-        console.error('Error connecting: ', err);
-    });
+  .login(process.env.TOKEN)
+  .then(() => {
+    console.log("Successfully Connected!");
+  })
+  .catch((err) => {
+    console.error("Error connecting: ", err);
+  });
