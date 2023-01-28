fc8d60e51af1d2684139b8f3a9c25c034e38c4fe
Daniel Roberto <sdanirobert@outlook.com>
2023-01-28 16:04:32 -0300

    Add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-import * as dotenv from "dotenv";
-import * as Discord from "discord.js";
-import { GatewayIntentBits } from "discord.js";
+import * as dotenv from 'dotenv';
+import * as Discord from 'discord.js';
+import { GatewayIntentBits } from 'discord.js';
 dotenv.config();
 
 const client = new Discord.Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
-client.on("ready", () => {
-  console.log(`Connected as ${client.user!.tag}!`);
+client.on('ready', () => {
+    console.log(`Connected as ${client.user!.tag}!`);
 });
 
 client
-  .login(process.env.TOKEN)
-  .then(() => {
-    console.log("Successfully Connected!");
-  })
-  .catch((err) => {
-    console.error("Error connecting: ", err);
-  });
-
+    .login(process.env.TOKEN)
+    .then(() => {
+        console.log('Successfully Connected!');
+    })
+    .catch((err) => {
+        console.error('Error connecting: ', err);
+    });
