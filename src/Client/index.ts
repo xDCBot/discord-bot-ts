8e934cc96d16dc4f21d4577deb59ba9b8648bfab
Daniel Roberto <sdanirobert@outlook.com>
2023-02-02 16:08:38 -0300

    Added Implementation of Commands and Events for Bot Discord

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,9 +1,21 @@
-import { CommandRegistrationOptions } from "./../Typings/Client";
-import { Client, GatewayIntentBits } from "discord.js";
+import { CommandRegistrationOptions } from "../Typings/Client";
+import {
+  ApplicationCommandDataResolvable,
+  Client,
+  Collection,
+  GatewayIntentBits,
+} from "discord.js";
+import { Command } from "../Typings/Command";
+import { readdirSync } from "fs";
+import path from "path";
 import * as dotenv from "dotenv";
+import Title from "../Utils/String/Title";
 dotenv.config();
 
 class Bot extends Client {
+  public commands: Collection<string, Command> = new Collection();
+  public events: Collection<string, Event> = new Collection();
+
   constructor() {
     super({
       intents: [
@@ -15,21 +27,50 @@ class Bot extends Client {
     });
   }
 
-  async registerCommands({ commands, guildId }: CommandRegistrationOptions) {}
+  async registerCommands({ commands, guildId }: CommandRegistrationOptions) {
+    guildId
+      ? this.guilds.cache.get(guildId)?.commands.set(commands)
+      : this.application?.commands.set(commands);
+  }
 
-  async registerModules() {}
-
-  async start() {
-    this.on("ready", () => {
-      console.log(`Connected as ${this.user?.tag}!`);
+  async registerModules() {
+    const slashCommands: ApplicationCommandDataResolvable[] = [];
+    const commandPath = path.join(__dirname, "..", "Commands");
+    const commandFiles = readdirSync(commandPath).flatMap((dir) => {
+      return readdirSync(`${commandPath}/${dir}`)
+        .filter((file) => file.endsWith(".ts"))
+        .map((file) => `${commandPath}/${dir}/${file}`);
     });
-    this.login(process.env.TOKEN)
-      .then(() => {
-        console.log("Successfully Connected!");
-      })
-      .catch((err) => {
-        console.error("Error connecting: ", err);
-      });
+
+    const promises = commandFiles.map(async (filePath) => {
+      const command = await import(path.resolve(filePath)).then(
+        (data) => data.slash
+      );
+      if (!command?.name) return;
+      this.commands.set(command.name, command);
+      slashCommands.push(command);
+      console.log(`[COMMAND]: ${Title(command.name)} Loaded!`);
+    });
+
+    await Promise.all(promises);
+  }
+
+  public async start() {
+    this.login(process.env.TOKEN);
+    this.registerModules();
+
+    const eventDirectory = path.join(__dirname, "..", "Events");
+    const eventFiles = readdirSync(eventDirectory);
+
+    for (const file of eventFiles) {
+      if (!file.endsWith(".ts")) {
+        continue;
+      }
+
+      const { event } = await import(`${eventDirectory}/${file}`);
+      this.events.set(event.name, event);
+      this.on(event.name, event.run.bind(null, this));
+    }
   }
 }
 
