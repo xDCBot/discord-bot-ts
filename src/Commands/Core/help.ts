57145d476944c23c07ceae5e174ce9f36ca11e7b
Syp.Filter() <gastomafo@gmail.com>
2023-02-03 16:29:08 -0300

    Add help to commands

--- src/Commands/Core/help.ts
+++ src/Commands/Core/help.ts
@@ -5,7 +5,7 @@ export const slash: Command = {
   description: "🗒️ Help menu.",
   run: ({ interaction }) => {
     return interaction.followUp({
-      content: `This code comes from a GitHub project (https://github.com/xDCBot/discord-bot-ts).\nThe use of this one is possible while keeping the credits for free.\nIf you find a bug, please contact us!\nDaniel R.#9999 and miukimuik#0062`,
+      content: `This code comes from a GitHub project (https://github.com/xDCBot/discord-bot-ts).\nThe use of this one is possible while keeping the credits for free.\nIf you find a bug, please contact us!\nmiukimuik#0062 and Daniel R.#9999`,
     });
   },
 };
