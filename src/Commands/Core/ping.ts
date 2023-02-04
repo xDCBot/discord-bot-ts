1414305dfd923f958a52497cbea8462df5593a33
Syp.Filter() <gastomafo@gmail.com>
2023-02-04 01:08:08 -0300

    Change ping description

--- src/Commands/Core/ping.ts
+++ src/Commands/Core/ping.ts
@@ -2,7 +2,7 @@ import { Command } from "../../Typings";
 
 export const slash: Command = {
   name: "ping",
-  description: "Calculates the ping of the bot.",
+  description: ":comet:Show the ms!.",
   run: ({ interaction }) => {
     return interaction.followUp({
       content: `:rocket:  Ping: \`${interaction.client.ws.ping} ms\`.`,
