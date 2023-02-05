7d8766296e648bab5eb8299dfc3d5377f56b73b6
Syp.Filter() <123755978+SypFilter@users.noreply.github.com>
2023-02-05 14:23:43 -0300

    Fixing help spacing error (#15)

--- src/Commands/Core/help.ts
+++ src/Commands/Core/help.ts
@@ -5,10 +5,10 @@ export const slash: Command = {
   description: "🗒️ Show help information.",
   run: ({ interaction }) => {
     return interaction.followUp({
-      content: `This code comes from a GitHub project (https://github.com/xDCBot/discord-bot-ts).\n
-      The use of this one is possible while keeping the credits for free.\n
-      If you find a bug, please contact us!\n
-      miukimuik#0062 and Daniel R.#9999`,
+      content: `This code comes from a GitHub project (https://github.com/xDCBot/discord-bot-ts).\n` +
+      `The use of this one is possible while keeping the credits for free.\n` +
+      `If you find a bug, please contact us!\n` +
+      `miukimuik#0062 and Daniel R.#9999`,
     });
   },
 };
